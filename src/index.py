233f8e6c007b85443592d8923f369acefa09757f
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-03-24 11:35:54 -0300

    Code cleanup and documentation

--- src/index.py
+++ src/index.py
@@ -5,38 +5,44 @@ from robots.text import robot
 
 class Content:
     def __init__(self):
-        self.searchTerm = ""
+        self.search_term = ""
         self.prefix = ""
-        self.searchContentOriginal = ""
-        self.sourceContentClean = ""
+        self.search_content_original = ""
+        self.source_content_clean = ""
         self.sentences = []
 
     def __str__(self):
-        return f"{ self.searchTerm }'s content"
+        return f"{ self.search_term }'s content"
 
 
 def start():
+    # Function designed to orchestrate the robots calls
+
     content = Content()
 
-    def askAndReturnSearch():
+    def ask_and_return_search():
+        # Asks for the Wikipedia search term and returns it
         return input("Type a Wikipedia search term:")
 
-    def askAndReturnPrefix():
+    def ask_and_return_prefix():
+        # Lists the search prefix pre-defined options and asks for the user to choose between them
         prefixes = ['Who is', 'What is', 'The history of']
         for index in range(0, 3):
             print(f"[{ index + 1 }] - { prefixes[index] }")
         print("[0] - exit")
-        selectedPrefixIndex = int(input("Choose one option:")) - 1
-        if selectedPrefixIndex >= 0:
-            return prefixes[selectedPrefixIndex]
+        selected_prefix_index = int(input("Choose one option:")) - 1
+        if selected_prefix_index >= 0:
+            return prefixes[selected_prefix_index]
         sys.exit(1)
 
-    
-    content.searchTerm = askAndReturnSearch()
-    content.prefix = askAndReturnPrefix()
+    # Stores the data provided from the user
+    content.search_term = ask_and_return_search()
+    content.prefix = ask_and_return_prefix()
 
+    # Starts the text robot passing the content object to it
     robot(content)
 
-    print(content)
+    # print(content)
 
+# Initialize the robots orchestrator
 start()
